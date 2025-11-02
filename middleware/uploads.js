d1496f03f28ba334990e7ef341c0d741e5d0ea74
ahaddev655 <ahad.dev.eng@gmail.com>
2025-11-02 17:24:02 +0500

    Storage destination changed

--- middleware/uploads.js
+++ middleware/uploads.js
@@ -1,11 +1,7 @@
+// middleware/uploads.js
 import multer from "multer";
 
-const storage = multer.diskStorage({
-    destination: "uploads/",
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname);
-    },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
