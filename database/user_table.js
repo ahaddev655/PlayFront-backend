cc78afd42728fe3174e5b1ac7f51b7ad5631a209
ahaddev655 <ahad.dev.eng@gmail.com>
2025-11-04 08:53:15 +0500

    Table fixed

--- database/user_table.js
+++ database/user_table.js
@@ -6,7 +6,7 @@ const createUserTable = async () => {
       CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(100) NOT NULL,
-        email VARCHAR(100) NOT NULL UNIQUE,
+        email VARCHAR(100) NOT NULL,
         password VARCHAR(255) NOT NULL,
         token VARCHAR(255) NOT NULL,
         profile_image VARCHAR(255),
